41ef56e8979f23f072f8270e2ff71c3fce8fe541
Stefan Ilie <ilie.stefan92@yahoo.com>
2013-11-06 01:35:03 +0200

    change of plans. cacat

--- src/Graf.java
+++ src/Graf.java
@@ -1,8 +1,12 @@
 import java.awt.List;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
-public class Graf {
+public class Graf 
+{
 
 	/**
 	 * @param args
@@ -11,9 +15,52 @@ public class Graf {
 	 */
 	public static void main(String[] args)
 	{
-		ArrayList<Integer> arrAdiacenta = new ArrayList<Integer>();
+		int nIsRead;
+		File file = new File("elodia.txt");
+		try
+		{
+			Scanner sc = new Scanner(file);
+			ArrayList<ArrayList<Integer>> arrColum = new ArrayList<ArrayList<Integer>>();
+			ArrayList<Integer> arrLine = new ArrayList<Integer>();
+			
+			while(sc.hasNextLine())
+			{
+				ArrayList<Integer> arrTemp = new ArrayList<Integer>();
+				while(sc.next() != "\n")
+				{
+					arrTemp.add(sc.nextInt());
+				}
+				arrLine = arrTemp;
+				arrColum.add(arrLine);
+				sc.nextLine();
+			}
+			
+			
+			/**
+			 * This prints the graph.
+			 */
+			for (ArrayList<Integer> nParser : arrColum)
+			{
+				for (Integer nNumber : arrLine) 
+				{
+					System.out.println(arrLine.indexOf(nNumber) + ": ");
+					if(nNumber != arrLine.get(arrLine.size()-1))
+					{
+						System.out.println(nNumber + "->");
+					}
+					else 
+					{
+						System.out.println(nNumber);
+					}
+				}
+			}
+		}
+		catch (FileNotFoundException ex)
+		{
+			System.out.println(ex);
+		}
+		}
+		
 		
 
 	}
-
-}
